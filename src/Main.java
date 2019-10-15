7cb8818afbfd1b5ead3575f72ed0dbbbffe3e60f
hhaskovic1 <hhaskovic1@etf.unsa.ba>
2019-10-15 20:17:50 +0200

    Function sumaCifara added.

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,16 @@ import java.util.Scanner;
 
 public class Main {
 
+    public static int sumaCifara(int n) {
+        int suma = 0;
+        while(n!=0) {
+            suma += n % 10;
+            n /= 10;
+        }
+        return suma;
+
+    }
+
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
